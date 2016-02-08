81aa6d0a1ffc5f18bccb1ac9c7ae24abdbec8b3e
JMNM <orrawoj@gmail.com>
2016-02-08 21:17:36 +0100

    Añadido ruta gps
    Una vez obtenida una localización de un código QR se muestra la ruta a
    seguir desde la posición actual.

--- app/src/main/java/com/example/appgpsqr/MainActivity.java
+++ app/src/main/java/com/example/appgpsqr/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.appgpsqr;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,12 +17,17 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.content.Intent;
 
+import java.util.Locale;
+
+import android.net.Uri;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView x;
     private TextView y;
     private double latitud;
     private double longitud;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
                 new IntentIntegrator(MainActivity.this).initiateScan();
             }
         });
-        y=(TextView) findViewById(R.id.textView);
-        x=(TextView) findViewById(R.id.textView2);
+        y = (TextView) findViewById(R.id.textView);
+        x = (TextView) findViewById(R.id.textView2);
     }
 
     @Override
@@ -56,25 +67,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode) {
-            case IntentIntegrator.REQUEST_CODE:
-            {
-                if (resultCode == RESULT_CANCELED){
-                }
-                else
-                {
+        switch (requestCode) {
+            case IntentIntegrator.REQUEST_CODE: {
+                if (resultCode == RESULT_CANCELED) {
+                } else {
                     //Recogemos los datos   que nos envio el código Qr/codigo de barras
                     IntentResult scanResult = IntentIntegrator.parseActivityResult(
                             requestCode, resultCode, data);
                     String coordenadas = scanResult.getContents();
-                    String[] res=coordenadas.split("_");
-                    if(res.length>=4) {
+                    String[] res = coordenadas.split("_");
+                    if (res.length >= 4) {
                         latitud = Double.parseDouble(res[1]);
                         longitud = Double.parseDouble(res[3]);
                     }
-                    x.setText(latitud+"");
-                    y.setText(longitud+"");
+                    x.setText(latitud + "");
+                    y.setText(longitud + "");
+
+                    LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    Criteria criteria = new Criteria();
+
+                    criteria.setAccuracy(criteria.ACCURACY_FINE);
+
+                    String proveedor = lm.getBestProvider(criteria, true);
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    Location pos = lm.getLastKnownLocation(proveedor);
+
+                    //String uri = String.format(Locale.ENGLISH, "geo:%f,%f", latitud, longitud);
+
+                    String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f(%s)&daddr=%f,%f(%s)", pos.getLatitude(), pos.getLongitude(), "Origen", latitud, longitud,"Destino");
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    this.startActivity(intent);
                 }
                 break;
             }
