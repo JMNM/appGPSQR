120c5c309c7887f5cf57ef6c236031b222219adf
JMNM <orrawoj@gmail.com>
2016-02-08 17:58:06 +0100

    Aplicación base añadido lector qr
    Aplicación base de Android Studio añadiendo lo necesario para leer
    códigos qr

---
+++ app/src/main/java/com/example/appgpsqr/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.appgpsqr;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+    private TextView x;
+    private TextView y;
+    private double latitud;
+    private double longitud;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new IntentIntegrator(MainActivity.this).initiateScan();
+            }
+        });
+        y=(TextView) findViewById(R.id.textView);
+        x=(TextView) findViewById(R.id.textView2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case IntentIntegrator.REQUEST_CODE:
+            {
+                if (resultCode == RESULT_CANCELED){
+                }
+                else
+                {
+                    //Recogemos los datos   que nos envio el código Qr/codigo de barras
+                    IntentResult scanResult = IntentIntegrator.parseActivityResult(
+                            requestCode, resultCode, data);
+                    String coordenadas = scanResult.getContents();
+                    String[] res=coordenadas.split("_");
+                    if(res.length>=4) {
+                        latitud = Double.parseDouble(res[1]);
+                        longitud = Double.parseDouble(res[3]);
+                    }
+                    x.setText(latitud+"");
+                    y.setText(longitud+"");
+                }
+                break;
+            }
+        }
+    }
+}
